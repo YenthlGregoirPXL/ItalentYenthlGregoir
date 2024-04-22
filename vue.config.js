feda3de8974e30996d841a220abdbc5772890719
Yenth'l Grégoir <93191140+YenthlGregoirPXL@users.noreply.github.com>
2024-04-22 16:23:34 +0200

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  publicPath: '/ItalentYenthlGregoir/'
+}
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/ItalentYenthlGregoir/" : "/",
-};
